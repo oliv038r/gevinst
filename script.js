b9e28e2605b322bafa8452099f3281404d29e291
oliv038r <oliv038r@stud.kea.dk>
2021-10-13 21:49:23 +0200

    justeringer igen

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
+/* const sections = document.querySelectorAll("section");
+
+sections.forEach((section) => {
+    const buttertitles = document.querySelector(".products-2 h2");
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: section,
+        start: "",
+        end: "",
+        markers: true,
+        scrub: true,
+        pin: true,
+    },
+})
+.from(buttertitles, {
+    opacity: 0,
+    duration: 1,
+    x: -100,
+})
+}); */
